12a18723be0287a4e1bb5ffcd7b16837bb8454ca
Carl-AndrewDizon <caydizon.student@ua.edu.ph>
2024-04-11 14:09:56 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+// function CalcButton({label}) {
+//   function handleClick() {
+//     alert('you clicked!');
+//   }
+
+//   return (
+//     <button  className="CalcButton" onClick={handleClick}>
+//       {label}
+//     </button>
+//   );
+// }
+
+function Dizon({display}) {
+  return (
+    <div className="Dizon">
+      {display}
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Dizon display={"hello world"}/>
+      
+      </div>
   );
 }
 
